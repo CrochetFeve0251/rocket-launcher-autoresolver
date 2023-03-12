13fb0044ff867cea4bc5e24ffe37a245d9f40d64
Cyrille C <18537428+CrochetFeve0251@users.noreply.github.com>
2023-03-12 14:12:26 +0100

    Added activators and deactivators

--- inc/ServiceProvider.php
+++ inc/ServiceProvider.php
@@ -2,7 +2,9 @@
 namespace RocketLauncherAutoresolver;
 
 use RocketLauncherAutoresolver\Services\DependencyTreeResolver;
+use RocketLauncherCore\Activation\HasActivatorServiceProviderInterface;
 use RocketLauncherCore\Container\AbstractServiceProvider;
+use RocketLauncherCore\Deactivation\HasDeactivatorServiceProviderInterface;
 
 class ServiceProvider extends AbstractServiceProvider
 {
@@ -14,6 +16,17 @@ class ServiceProvider extends AbstractServiceProvider
     }
 
     protected function get_root_classes() {
-        return array_merge($this->get_init_subscribers(), $this->get_admin_subscribers(), $this->get_common_subscribers(), $this->get_front_subscribers());
+
+        $roots = array_merge($this->get_init_subscribers(), $this->get_admin_subscribers(), $this->get_common_subscribers(), $this->get_front_subscribers());
+
+        if($this instanceof HasActivatorServiceProviderInterface) {
+            $roots = array_merge($roots, $this->get_activators());
+        }
+
+        if($this instanceof HasDeactivatorServiceProviderInterface) {
+            $roots = array_merge($roots, $this->get_deactivators());
+        }
+
+        return $roots;
     }
 }
